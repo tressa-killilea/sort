fc00a142648afb1c3bd55f63713c16bee66e91da
Tressa Killilea <tressa@Tressas-MacBook-Pro.local>
2023-09-22 11:14:07 -0400

    remove unused code

--- src/app/services/songs.service.ts
+++ src/app/services/songs.service.ts
@@ -3,22 +3,19 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class Songs {
+  constructor(private http: HttpClient) {}
 
-    constructor (private http: HttpClient) { }
+  getPlaylist(token: string, url: string, id: string) {
+    if (url === '')
+      url = 'https://api.spotify.com/v1/playlists/' + id + '/tracks';
 
-    getPlaylist(token: string, url:string, id:string) {
-        var temp:any = []
-        if(url === '') url = 'https://api.spotify.com/v1/playlists/'+id+'/tracks';
+    var headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    return this.http.get(url, { observe: 'response', headers: headers });
+  }
+  getPlaylistInfo(token: string, url: string, id: string) {
+    if (url === '') url = 'https://api.spotify.com/v1/playlists/' + id;
 
-        var headers = new HttpHeaders().set('Authorization', 'Bearer '+token);
-        return this.http.get(url, { observe: 'response' , headers: headers});
-   }   
-   getPlaylistInfo(token: string, url:string, id:string) {
-        var temp:any = []
-        if(url === '') url = 'https://api.spotify.com/v1/playlists/'+id;
-
-        var headers = new HttpHeaders().set('Authorization', 'Bearer '+token);
-        return this.http.get(url, { observe: 'response' , headers: headers});
- 
-    }   
+    var headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+    return this.http.get(url, { observe: 'response', headers: headers });
+  }
 }
